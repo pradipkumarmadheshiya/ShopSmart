36fa29df32b530fbb3bc227038f39e1361a01de1
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2025-07-19 16:29:30 +0530

    update in add to cart in deal of the day

--- client/src/components/DealOfTheDay .jsx
+++ client/src/components/DealOfTheDay .jsx
@@ -89,7 +89,7 @@ function DealOfTheDay() {
               <span className="text-2xl font-bold">$199.99</span>
               <span className="text-lg text-gray-500 line-through ml-2">$249.99</span>
             </div>
-            <button onClick={()=>addToCart()}
+            <button onClick={()=>addToCart(1)}
             className="bg-gray-900 hover:bg-black text-white font-medium py-3 px-6 rounded-lg flex items-center justify-center cursor-pointer">
               <ShoppingCart size={20} className="mr-2" />
               Add to Cart
